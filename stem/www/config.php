e5d7beb7d1434eb2a31242bd614b8bc258c2eb14
root <root@ktomega.net>
2013-09-25 00:17:38 -0700

    Forgot the config file

---
+++ stem/www/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = 'localhost';
+$dbport = 'Database Port';
+$dbname = 'Database Name';
+$dbuser = 'Database Username';
+$dbpass = 'Database Password';
+
+$rt = '/';
+$rtexact = $_SERVER['DOCUMENT_ROOT'] . '/';
