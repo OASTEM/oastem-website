31fd8b83247b3882fdc8daee58b4fcca5b6acf4f
root <root@ktomega.net>
2013-09-25 00:06:31 -0700

    NO

--- stem/www/include/trimSessions.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('../config.php');
-require('./mysql.php');
-
-$db = new MySQL($dbhost, $dbport, $dbname, $dbuser, $dbpass);
-
-print time() - 60*60*24*7;
-
-$db->query("DELETE FROM `kt_sessions` WHERE `session_last_visit` < " . time() - 60*60*24*7);
