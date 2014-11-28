7a16ba9dded1151bdd3ad365938cd7730dd1f746
Oxford Academy STEM <stem@ktomega.net>
2014-11-27 16:50:20 -0800

    Changed configuration files.

---
+++ www/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+// Sample values provided.
+$dbhost = 'your-hostname';
+$dbport = '3306';
+$dbname = 'database_name';
+$dbuser = 'user_name';
+$dbpass = 'user_password';
+
+$rt = '/';
+$rtexact = $_SERVER['DOCUMENT_ROOT'] . '/';
