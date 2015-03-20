0127bc6f65934bdf5f4238b5152861a86dadac43
bnguyen654 <bnguyen9469@yahoo.com>
2015-03-20 00:16:00 -0700

    Put functions in media.

--- www/media/index.php
+++ www/media/index.php
@@ -5,8 +5,14 @@ $templ->setTitle('Media');
 $templ->render('header');
 ?>
 <script src="media.js"></script>
+<link href="media.css" rel="stylesheet">
 
 <div id="media-content">
+</div>
+
+<div id="media-control">
+<button id="prev">Previous</button>
+<button id="next">Next</button>
 </div>
 
 <?php
