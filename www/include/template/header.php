e76ead32d9bb712cedc159a166335bfc74ace7fa
Kevin Tran <me@kevintran.codes>
2015-06-08 22:02:36 -0700

    Fix some things:
    - Made div#content auto-width instead of 1000px.
    - Hide #sm-wrap in addition to @social-media.
    - Make the posts full-width instead of 90%.
    - Change html extension to php.

--- www/include/template/header.php
+++ www/include/template/header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title><?php print Template::getInstance()->getVar('header', 'title'); ?> &raquo; OA STEM</title>
 <script type="text/javascript" src="/js/jquery.js"></script>
 <script type="text/javascript" src="/js/jquery-ui.js"></script>
