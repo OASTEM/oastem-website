cf842dad1b8c0b5f829f903146a44240f79eafdb
PeterYang1625 <peteryang1625@gmail.com>
2015-07-01 22:06:10 -0700

    MY HANDS ARE TYPING WORDS
    Styled edit/delete buttons and fixed vertical positioning problem for
    posts which the user didn't have edit permissions.

--- www/ajax_posts.php
+++ www/ajax_posts.php
@@ -116,9 +116,9 @@
 				<div id='post" . $row['pid'] . "' class='post-wrapper' data-pid='" . $row['pid'] . "' data-cid='" . $op['cid'] . "'>
 					<div class='post-header' style='background-color:" . $cat['color1'] . "'>";
 				if($logged_in && ($row['uid'] == $_SESSION['uid'] || $_SESSION['sa'] == 1)){ //check permissions
-					echo   "<button type='button' style='float: left' class='edit-button'>Edit</button>
-                           <button type='button' style='float: right' class='delete-button'>Delete</button>";
-				}else{
+					echo   "<button type='button' style='float: left' class='edit button'>Edit</button>
+                           <button type='button' style='float: right' class='delete button'>Delete</button>";
+				}else if (!$logged_in){
 						echo"
 							<h2 class='category'>" . $cat['name'] . "</h2>
 							<p class='timestamp'>" . date('F j',$tstring) . ", <time class='post-time' datetime='" . $row['time']
