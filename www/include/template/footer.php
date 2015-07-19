fb928fd37085ccadd17af39a663b509dd6468460
PeterYang1625 <peteryang1625@gmail.com>
2015-07-18 23:36:17 -0700

    sectioned
    Still deleting some css and adding some

--- www/include/template/footer.php
+++ www/include/template/footer.php
@@ -1,15 +1,11 @@
 	</div>
     <footer>
-		<ul id="nav">
-			<li><a href="contact.html">Contact</a></li>
-			<li><a href="about.html">About</a></li>
-		</ul>
-
-		<div class="right">
+            <a href="https://www.facebook.com/OASTEM"><img id="social-media"src="../../images/facebook.svg"></a>
+            <a href="https://github.com/oastem"><img id="social-media" src="../../images/github.svg"></a>
+            <a href="http://ask.fm/OASTEM"><img id="social-media" src="../../images/ask_fm.svg"></a>
+            <a href="../../contact/">Contact</a>
+            <a href="../../about/"</a>
 			&copy; 2012-2015
-		</div>
-
-		<div style="clear:both"></div>
 	</footer>
 </div>
 </body>
