b7f10a2d36c2ec4d3674b3dae828d29d7e401e05
PeterYang1625 <peteryang1625@gmail.com>
2015-11-22 12:17:47 -0800

    Centered Media Page

--- www/media/index.php
+++ www/media/index.php
@@ -5,7 +5,7 @@ $templ->setTitle('Media');
 $templ->render('header');
 ?>
 <link href="media.css" rel="stylesheet">
-<iframe src="https://drive.google.com/embeddedfolderview?id=0ByDyUerJE7tFfjdlVlJyUktEaVgtRkN1ak9KdjBoOEVPbVZrTkNjSEw4UjFiR0Z6NnF2WUk#grid" style="width: 1000px; height: 1400px; margin: 0 auto; display: block"></iframe>
+<iframe src="https://drive.google.com/embeddedfolderview?id=0ByDyUerJE7tFfjdlVlJyUktEaVgtRkN1ak9KdjBoOEVPbVZrTkNjSEw4UjFiR0Z6NnF2WUk#grid" style="width: 900px; height: 1400px; margin: 0 auto; display: block"></iframe>
 
 <?php
 $templ->render('footer');
